819feb23c7d6cde7f737e59a5bad78682cc4a975
Spencer Cloughley <spencercloughley@gmail.com>
2023-09-20 15:53:51 -0400

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.st1178551labweek2 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.st1178551labweek2 to javafx.fxml;
+    exports com.example.st1178551labweek2;
+}
